9ffd92437e82e1efa03f8347763cd06a1f43b5cb
Dániel Agg <daniel.agg@outlook.com>
2019-05-05 17:42:13 +0200

    Forcing scroll to top upon ProjectDetails component load, desktop styling alignments of component

--- src/components/portfolio/ProjectDetails.js
+++ src/components/portfolio/ProjectDetails.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "../shared/Footer";
 import ThemeSelector from "../shared/ThemeSelector";
 import { Link } from "react-router-dom";
@@ -6,6 +6,8 @@ import { Consumer } from "../../context";
 import "./ProjectDetails.css";
 
 const ProjectDetails = props => {
+  useEffect(() => window.scrollTo(0, 0), []);
+
   return (
     <Consumer>
       {value => {
